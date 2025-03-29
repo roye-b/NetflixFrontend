520d00a114f0768496359822c345c205757710cd
roye-b <rbega2@gmail.com>
2025-03-29 14:46:25 -0700

    change authors name

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <p>
         Made by{' '}
         <a href='https://github.com/ayushiee/nextflix' target='_blank' rel='noreferrer'>
-          <b>Ayushi Gupta</b>
+          <b>Ayushi Gupta and roye</b>
         </a>
       </p>
     </div>
